83ca30dba9592996ac781ccebbe417d3e4ed9255
HYEJI-WOO <whj1939@naver.com>
2022-11-09 15:36:20 +0900

    add

--- pro09/src/main/java/sec02/ex01/GetCookieValue.java
+++ pro09/src/main/java/sec02/ex01/GetCookieValue.java
@@ -2,6 +2,7 @@ package sec02.ex01;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -11,22 +12,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/set")
-public class SetCookieValue extends HttpServlet {
+@WebServlet("/get")
+public class GetCookieValue extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
-		Date date = new Date(); // 현재 날짜
+		Cookie[] cookies = request.getCookies();
 		
-		// 쿠키 생성
-		Cookie c1 = new Cookie("jsp","JSP프로그래밍");
-		Cookie c2 = new Cookie("oracle", "오라클데이터베이스");
-		response.addCookie(c1); // 생성된 쿠키를 브라우저로 전송
-		response.addCookie(c2);
+		if(cookies!=null) {
+			for(Cookie c : cookies) {
+				out.print("쿠키 이름 : " + c.getName());
+				out.print(" & 쿠키 값 : " + c.getValue() + "<br>");
+			}
+			
+		} else {
+			out.print("지정된 쿠키 없음");
+		}
 		
-		out.print("현재 시간 : " + date + "<br>");
-		out.print("문자열을 Cookie에 저장");
 	}
 
 
