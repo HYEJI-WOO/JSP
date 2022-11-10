3eca7be730bf119caa7cb5aee81436a4c02204e2
HYEJI-WOO <whj1939@naver.com>
2022-11-10 12:42:58 +0900

    add

---
+++ pro09/src/main/java/sec03/ex01/SessionTest3.java
@@ -0,0 +1,30 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/sess3")
+public class SessionTest3 extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		
+		HttpSession session = req.getSession();
+		if(session.isNew()) {
+			out.print("세션 생성");
+		}
+		session.invalidate(); // 세션 삭제
+		
+	}
+
+}
