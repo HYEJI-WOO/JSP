803ec2c87c6cc19fdfa684c58a38a51af9583f06
HYEJI-WOO <whj1939@naver.com>
2022-11-14 13:52:21 +0900

    add

--- member_pro01/src/main/java/test/MemberDaoTest.java
+++ member_pro01/src/main/java/test/MemberDaoTest.java
@@ -7,13 +7,7 @@ import model.MemberVO;
 public class MemberDaoTest {
 	public static void main(String[] args) {
 		MemberDao dao = MemberDaoImpl.getInstance();
-		MemberVO vo = new MemberVO(
-				0,
-				"dori",
-				"1234",
-				"도리",
-				"dori@naver.com",
-				null);
+		MemberVO vo = new MemberVO(0,"dori","1234","도리","dori@naver.com",null);
 		dao.addMember(vo);
 		System.out.println(dao.memberList());
 	}
