99def499bdef19554e454d4061d5007a03243ac8
HYEJI-WOO <whj1939@naver.com>
2022-11-25 12:42:18 +0900

    add

--- board_ex/src/main/java/com/dao/BoardDAO.java
+++ board_ex/src/main/java/com/dao/BoardDAO.java
@@ -14,14 +14,13 @@ import com.domain.BoardVO;
 
 public class BoardDAO {
 
-	private DataSource dataSource;
+private DataSource dataSource;
 	
 	public BoardDAO() {
 		try {
 			Context ctx = new InitialContext();
 			Context envCtx = (Context) ctx.lookup("java:/comp/env");
 			dataSource = (DataSource) envCtx.lookup("jdbc/oracle");
-			System.out.println(dataSource);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,7 +40,8 @@ public class BoardDAO {
 						.bno(rs.getInt("bno"))
 						.title(rs.getString("title"))
 						.content(rs.getString("content"))
-						.writer(rs.getString("writer")).build();
+						.writer(rs.getString("writer"))
+						.writeDate(rs.getDate("writeDate")).build();
 				list.add(vo);
 			}
 			
@@ -50,5 +50,76 @@ public class BoardDAO {
 		}
 		
 		return list;
+	}
+	
+	public void addArticle(BoardVO vo) {
+		String query = "insert into board_t(bno, title, content, writer) values(bno_seq.nextval, ?, ?, ?)";
+		try(
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(query);
+		){
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setString(3, vo.getWriter());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 회원상세정보
+	public BoardVO findArticle(int bno) {
+		BoardVO vo = null;
+		String query = "SELECT * FROM board_t WHERE bno=?";
+		try(
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(query);
+		){
+			pstmt.setInt(1, bno);
+			try(ResultSet rs = pstmt.executeQuery();){
+				if(rs.next()) {
+					vo = BoardVO.builder()
+						.bno(rs.getInt("bno"))
+						.title(rs.getString("title"))
+						.content(rs.getString("content"))
+						.writer(rs.getString("writer"))
+						.writeDate(rs.getDate("writeDate")).build();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return vo;
+	}
+		
+	// 회원수정
+	public void modArticle(BoardVO vo) {
+		String query = "UPDATE board_t SET title=?, content=? WHERE bno=?";
+		try(
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(query);
+			){
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setInt(3, vo.getBno());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+		
+	// 회원삭제
+	public void delArticle(int bno) {
+		String query = "DELETE FROM board_t WHERE bno=?";
+		try(
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(query);
+		){
+			pstmt.setInt(1, bno);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
